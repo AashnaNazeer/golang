97bdf66b0958de192016177d4e641032d6a72e1d
AashnaNazeer <aashnanazeer127@gmail.com>
2026-07-16 14:56:36 +0530

    largestof3.go

---
+++ largestof3.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c int
+
+	fmt.Print("Enter three numbers: ")
+	fmt.Scan(&a, &b, &c)
+
+	largest := a
+
+	if b > largest {
+		largest = b
+	}
+
+	if c > largest {
+		largest = c
+	}
+
+	fmt.Println("Largest =", largest)
+}
